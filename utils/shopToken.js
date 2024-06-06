5375e483939ebd7b49f58aad9f909ff1c9852234
fadhilZamanNow <fadhilisfadhillah@gmail.com>
2024-06-07 04:47:34 +0700

    coba5

--- utils/shopToken.js
+++ utils/shopToken.js
@@ -7,6 +7,8 @@ const sendShopToken = (user, statusCode, res) => {
     const options = {
         expires : new Date(Date.now() + 90 + 24 * 24 * 60 * 60 * 1000),
         httpOnly : true,
+        secure : true,
+        sameSite : "none"
     }
 
     res.status(statusCode).cookie("seller_token", token, options).json({
