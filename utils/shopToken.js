df2493305bb6eb4432e597a5870e4aeb0495fd46
fadhilZamanNow <fadhilisfadhillah@gmail.com>
2024-06-07 04:50:26 +0700

    coba6

--- utils/shopToken.js
+++ utils/shopToken.js
@@ -2,13 +2,14 @@ const sendShopToken = (user, statusCode, res) => {
     const token = user.getJwtToken();
 
 
-    // option untuk cookies
+    // option untuk cookies, nambahin secure sama samesite
 
     const options = {
         expires : new Date(Date.now() + 90 + 24 * 24 * 60 * 60 * 1000),
         httpOnly : true,
         secure : true,
         sameSite : "none"
+
     }
 
     res.status(statusCode).cookie("seller_token", token, options).json({
