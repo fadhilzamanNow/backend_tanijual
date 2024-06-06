78918e13a399b537c28eda28441b1125e286ad88
fadhilZamanNow <fadhilisfadhillah@gmail.com>
2024-06-07 04:28:11 +0700

    coba1

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -6,9 +6,9 @@ const sendToken = (user, statusCode, res) => {
 
     const options = {
         expires : new Date(Date.now() + 90 + 24 * 24 * 60 * 60 * 1000),
+        Secure : true,
         httpOnly : true,
         sameSite: "none",
-        Secure : true
     }
 
     res.status(statusCode).cookie("token", token, options).json({
