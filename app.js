7fe263ff788840b38f936170979ca52123f335df
fadhilZamanNow <fadhilisfadhillah@gmail.com>
2024-06-07 13:00:31 +0700

    benerin cors 2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(cookieParser());
 app.use(cors({  
-    origin : "https://frontend-tanijual.vercel.app",
+    origin : "https://frontend-tanijual.vercel.app/",
     credentials : true
 }));
 /* app.use("/",express.static("uploads")); */
